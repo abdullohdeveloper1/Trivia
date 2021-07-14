88fcd53fdd14ef5805ccc2f8440509c483c2011c
Abdulloh <abdullohjorayev1@mail.ru>
2021-07-14 10:30:00 +0300

    Trivia API

---
+++ backend/test_flaskr.py
@@ -0,0 +1,32 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+from flaskr import create_app
+from models import setup_db, Question, Category
+
+
+class TriviaTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "trivia_test"
+        self.database_path = "postgresql://{}:{}@{}/{}".format('postgres', '12', 'localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            self.db.create_all()
+    
+    def tearDown(self):
+        pass
+
+    """
+    TODO
+    Write at least one test for each test for successful operation and for expected errors.
+    """
+
+if __name__ == "__main__":
+    unittest.main()
